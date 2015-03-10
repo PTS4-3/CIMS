02db3479155c8f3f5ffb7ff4d0e65fec9ba064d2
lotje1990 <lotje1990@hotmail.com>
2015-03-10 12:35:57 +0100

    datamanager2

--- src/ServerApp/DatabaseManager.java
+++ src/ServerApp/DatabaseManager.java
@@ -6,10 +6,72 @@
 
 package ServerApp;
 
+import Shared.SortedData;
+import Shared.UnsortedData;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Linda
  */
 public class DatabaseManager {
     
+   private Connection conn;
+   
+   public DatabaseManager()
+   {
+       
+   }
+   
+   public boolean insertToUnsortedData(UnsortedData data)
+   {
+       boolean succeed = false;
+       
+       return succeed;
+   }
+   
+   public UnsortedData getFromUnsortedData()
+   {
+       return null;
+   }
+   
+   public boolean insertToSortedData(SortedData data)
+   {
+       boolean succeed = false;
+       
+       return succeed;
+   }
+   
+   public SortedData getFromSortedData()
+   {
+       return null;
+   }
+   
+   
+   private void openConnection()
+   {
+       try{
+           conn = DriverManager.getConnection("jdbc:oracle:thin:@fhictora01.fhict.local:1521:fhictora", "dbi294542", "vl4ldKvhy8");
+           System.out.println("Connection open succeeded");
+       }
+       catch(SQLException ex)
+       {
+           System.out.println("Connection open failed: " + ex);
+       }
+       
+   }
+   private void closeConnection()
+   {
+       try{
+           conn.close();
+           System.out.println("Connection close succeeded");
+       }
+       catch(SQLException ex)
+       {
+           System.out.println("Connection close failed: " + ex);
+       }
+       
+   }
 }
