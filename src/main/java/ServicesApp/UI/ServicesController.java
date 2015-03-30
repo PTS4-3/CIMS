3e169c5aee6628b3b0792c5b216b4c77f99a97c1
xChocoMel <melaniekwetters@hotmail.com>
2015-03-30 14:31:13 +0200

    tabindex

--- src/main/java/ServicesApp/UI/ServicesController.java
+++ src/main/java/ServicesApp/UI/ServicesController.java
@@ -118,7 +118,6 @@ public class ServicesController implements Initializable {
                             setText(item.toString());
 
                             if (item instanceof IDataRequest) {
-                                //@KittyPussyCat
                                 setTextFill(Color.RED);
                             } else if (item instanceof ISortedData) {
                                 setTextFill(Color.BLACK);
