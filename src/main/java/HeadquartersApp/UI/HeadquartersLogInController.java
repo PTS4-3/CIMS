21f6f99f488354e938101c081f90822f29516f33
bartbouten <bart.bouten@student.fontys.nl>
2015-04-21 11:45:22 +0200

    aanpassingen in fxcontrollers

--- src/main/java/HeadquartersApp/UI/HeadquartersLogInController.java
+++ src/main/java/HeadquartersApp/UI/HeadquartersLogInController.java
@@ -15,6 +15,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
@@ -72,11 +73,16 @@ public class HeadquartersLogInController implements Initializable {
             //USername password, controleren
             //user opvragen
             //doorsturen naar nieuw scherm
-            try{
-        this.main.goToHeadquarters(connectionManager, user);
-        } catch (Exception ex) {
-            Logger.getLogger(HeadquartersLogInController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+            
+            if(user != null) {
+                try{
+                    this.main.goToHeadquarters(connectionManager, user);
+                } catch (Exception ex) {
+                    Logger.getLogger(HeadquartersLogInController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else {
+                showDialog("Foutmelding", "User bestaat niet", true);
+            }
 
         } catch (NetworkException ex) {
             Logger.getLogger(HeadquartersLogInController.class.getName()).log(Level.SEVERE, null, ex);
@@ -88,5 +94,33 @@ public class HeadquartersLogInController implements Initializable {
         if (this.connectionManager != null) {
             this.connectionManager.close();
         }
+    }
+    
+    public void showDialog(String title, String melding, boolean warning)
+    {
+        Alert alert = null;
+        
+        if (warning)
+        {
+            alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("FoutMelding");
+        }
+        else
+        {
+            alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Melding");            
+        }     
+        
+        if (!title.isEmpty())
+        {
+            alert.setHeaderText(title);
+        }
+        else
+        {
+            alert.setHeaderText(null);
+        }
+        
+        alert.setContentText(melding);
+        alert.showAndWait();
     }
 }
